fdc2a4f765953ea8d196473e3a11da62d97a08e1
Quentin Plet <quentin.plet@gmail.com>
2023-12-20 19:22:36 +0100

    fix styled component bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import "../../App.css";
+import styled from "styled-components";
 
 const Header = () => {
   const [Toggle, setToggle] = useState(false);
