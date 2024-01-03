8735102261fe3ae2b2431c4951d3e7466bb58495
Quentin Plet <quentin.plet@gmail.com>
2024-01-03 20:14:04 +0100

    ✨ Add About Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
---
+++ src/components/About/Info.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const Info = () => {
+  return (
+    <InfoStyled className="about__info grid">
+      <div className="about__box">
+        <i className="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">8 Years Working</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">48 + Projects</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </InfoStyled>
+  );
+};
+
+export default Info;
+
+const InfoStyled = styled.div``;
