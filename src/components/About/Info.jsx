a4f7d2ff281f49c09e0896f5ddf24ba5a72c5b0c
Quentin Plet <quentin.plet@gmail.com>
2024-01-03 22:04:20 +0100

    ✨ Add Skills Component

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -11,13 +11,13 @@ const Info = () => {
       </div>
 
       <div className="about__box">
-        <i class="bx bx-briefcase-alt about__icon"></i>
+        <i className="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
         <span className="about__subtitle">48 + Projects</span>
       </div>
 
       <div className="about__box">
-        <i class="bx bx-support about__icon"></i>
+        <i className="bx bx-support about__icon"></i>
         <h3 className="about__title">Support</h3>
         <span className="about__subtitle">Online 24/7</span>
       </div>
